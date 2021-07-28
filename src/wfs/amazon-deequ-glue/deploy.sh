ca2587dd838762dcf419490a6e23aadc4358e27f
vijay75252 <42190210+vijay75252@users.noreply.github.com>
2021-07-27 23:25:14 -0500

    Update deploy.sh

--- src/wfs/amazon-deequ-glue/deploy.sh
+++ src/wfs/amazon-deequ-glue/deploy.sh
@@ -108,7 +108,7 @@ if ! aws cloudformation describe-stacks --profile $PROFILE  --region $REGION --s
       ParameterKey=pCreateFrontEnd,ParameterValue=$UI_FLAG \
       ParameterKey=pEnv,ParameterValue=$ENV \
       ParameterKey=Stage,ParameterValue=$ENV \
-      ParameterKey=pCodeBranch,ParameterValue=$CODEBRANCH \      
+      ParameterKey=pCodeBranch,ParameterValue=$CODEBRANCH \
       ParameterKey=pRedShiftSecretName,ParameterValue=$REDSHIFTSECRET \
     --tags file://$DIRNAME/tags.json \
     --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND"
